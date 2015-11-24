302f1e9d379be40f29afc697ebb6f0a4f426ea8e
junhao-wang <wangjh2@mcmaster.ca>
2015-11-24 00:18:54 -0500

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -6,77 +6,86 @@ import java.util.Scanner;
 public class ShoppingCart extends User{
 	
 	private ArrayList<String> content;
-	
+	//constructor for admins (never used)
 	public ShoppingCart(String usr,String pwd){
-		super(usr,pwd);
-		String fileName = "Cart_"+usr+".txt";
-		File temp = new File(fileName);
-		if (!temp.exists()){
+		super(usr,pwd);//calls constructor for user with username and password
+		String fileName = "Cart_"+usr+".txt";//computes the name of the shopping cart file
+		File temp = new File(fileName);//creates a file object
+		if (!temp.exists()){//if the file doesnt exist already
 			try {
-				PrintStream create = new PrintStream(temp);
-				create.close();
-			} catch (FileNotFoundException e) {
+				PrintStream create = new PrintStream(temp);//create the file with a printstream
+				create.close();//close the printstream
+			} catch (FileNotFoundException e) {//this block should not be reached
 				e.printStackTrace();
 			}
 		}
-		fileRead(this.content,fileName);	
+		fileRead(this.content,fileName);//read the content of the shopping cart file into the content array	
 	}
+	//constructor for regular users
 	public ShoppingCart(String usr){
-		super(usr);
-		String fileName = "Cart_"+usr+".txt";
-		File temp = new File(fileName);
-		if (!temp.exists()){
+		super(usr);//calls the constructor in User with just the username
+		String fileName = "Cart_"+usr+".txt";//computes the shopping cart filename
+		File temp = new File(fileName);//creates an object for the file
+		if (!temp.exists()){//check if the file actually exists, if not
 			try {
-				PrintStream create = new PrintStream(temp);
-				create.close();
-			} catch (FileNotFoundException e) {
+				PrintStream create = new PrintStream(temp);//initialize a print stream to make the file
+				create.close();//close the print stream
+			} catch (FileNotFoundException e) {//this block should not be reached since the file does not exist before creation and thus no access errors should be raised
 				e.printStackTrace();
 			}
 		}
-		fileRead(this.content,fileName);
+		//at this point the shopping cart file must exist by now, if it wasnt before, the last code block created it
+		fileRead(this.content,fileName);//reads the shopping cart file to the content arraylist
 	}
 	
+	//returns the static array representation of the contents of the shopping cart
 	public String[] getContent(){
-		return this.content.toArray(new String[this.content.size()]);
+		return this.content.toArray(new String[this.content.size()]);//converts the content arraylist to a string[] array
 	}
 	
+	//adds an item to the cart, the corresponding deduction from the store is handled by the UserInterface class, here
+	//we are just concerned with add the item to the cart.
 	public void addItem(Item itm,int quant){
-		int index = indexItem(itm);
-		if ( index == -1){
-			String temp = "";
-			temp += String.valueOf(itm.getsNo())+",";
-			temp += itm.getName()+",";
+		int index = indexItem(itm);//tries to see if some of the items are already in the cart
+		if ( index == -1){//item is not in the cart
+			//formats a string to represent the item in the format of the shopping cart txt file
+			String temp = "";//start with empty string
+			temp += String.valueOf(itm.getsNo())+",";// temp = "sNo,"
+			temp += itm.getName()+",";//temp = "sNo,nameOfItem,"
 			String date = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
-			temp += date+",";
-			temp += String.valueOf(quant);
-			this.content.add(temp);
-		}else{
-			String temp = this.content.get(index);
-			String[] tmp = temp.split(",");
-			String newDate = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
-			int newQuant = Integer.parseInt(tmp[3])+quant;
-			temp = String.valueOf(itm.getsNo()) +","+itm.getName()+","+newDate+","+String.valueOf(newQuant);
-			this.content.set(index, temp);
+			temp += date+",";//temp = "sNo,nameOfItem,DD/MM/YYYY,"
+			temp += String.valueOf(quant);//temp = "sNo,nameOfItem,DD/MM/YYYY,quantity"
+			this.content.add(temp);//addds the string to the array list
+		}else{//if some of the item is already in the cart
+			String temp = this.content.get(index);//find the corresponding string
+			String[] tmp = temp.split(",");//split that string into fields
+			String newDate = new SimpleDateFormat("dd/MM/yyyy").format(new Date());//set the date to the current time
+			int newQuant = Integer.parseInt(tmp[3])+quant;//adds the quantity to the pre-existing quantity
+			temp = String.valueOf(itm.getsNo()) +","+itm.getName()+","+newDate+","+String.valueOf(newQuant);//create the updated string
+			this.content.set(index, temp);//replace the old string at index with the new string
 		}
-		this.writeCart();
+		this.writeCart();//writes the changes to the shopping cart to the file
 		
 	}
-	
+	//tries to find an item in the shopping cart
+	//if found, returns its location (index in the array)
+	//if not found, return a -1
 	public int indexItem(Item itm){
-		String[] temp = this.getContent();
-		for (int i = 0;i<temp.length;i++){
-			String name = temp[i].split(",")[1].trim();
-			if (itm.getName() == name){
-				return i;
+		String[] temp = this.getContent();//dumps the content of the cart into a temporary string array
+		for (int i = 0;i<temp.length;i++){//iterates through the strings in the temp array
+			String name = temp[i].split(",")[1].trim();//extract the name of the item from the string
+			if (itm.getName() == name){//if the extracted name matches the name of the item we are looking for
+				return i;//return the index of the item in the shopping cart
 			}
 		}
-		return -1;
+		return -1;//if we cannot find the item in the shopping cart, return -1
 	}
+	//this function simply clears the cart
 	public void clearCart(){
-		this.content.clear();
-		this.writeCart();
+		this.content.clear();//removes everything from the content array list
+		this.writeCart();//writes the changes to the txt file
 	}
-	
+	//This function writes the shopping cart in its current state to the corresponding txt file
 	public void writeCart(){
 		String fileName = "Cart_"+this.getName()+".txt";
 		try {
