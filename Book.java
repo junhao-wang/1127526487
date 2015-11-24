a4afb2637d55d5c8b1d4da69699cefee48d4a65e
Colin Vandenhof <colinvandenhof@gmail.com>
2015-11-24 00:41:29 -0500

    Update Book.java, CD.java

--- Book.java
+++ Book.java
@@ -5,7 +5,7 @@ public class Book extends Readable{ //class Book is a subclass of Readable
 		super(sNo, name, authorName, price, quantity);//inherit constructors from Readable
 		
 	}
-	public int getPrice(){
+	public int getPrice(){ //getter method to return Price of Book as an int.
 		return super.getPrice(); //+super.getPrice()/50;
 		/*Note: it DOES NOT MAKE SENSE to add the 2% environmental fee here!
 		 *You print the price of each item and the total environmental fee SEPERATELY
@@ -13,7 +13,7 @@ public class Book extends Readable{ //class Book is a subclass of Readable
 		 *throughout the check out process.
 		*/
 	}
-	public String getType(){
+	public String getType(){ //getter method to get Readable type as a String
 		return "Book";  //method returns the Readable type as a String
 	}
 	
--- CD.java
+++ CD.java
@@ -1,11 +1,11 @@
 
-public class CD extends Audio {
+public class CD extends Audio { //class CD is a subclass of Audio
 
 	public CD(int sNo, String name, String artistName, int price, int quantity) {
-		super(sNo, name, artistName, price, quantity);
+		super(sNo, name, artistName, price, quantity); //inherit constructors from Audio
 		// TODO Auto-generated constructor stub
 	}
-	public int getPrice(){
+	public int getPrice(){ //getter method to return Price of CD as an int
 		return super.getPrice(); //+super.getPrice()/50;
 		/*Note: it DOES NOT MAKE SENSE to add the 2% environmental fee here!
 		 *You print the price of each item and the total environmental fee SEPERATELY
@@ -13,8 +13,8 @@ public class CD extends Audio {
 		 *throughout the check out process.
 		*/
 	}
-	public String getType(){
-		return "CD";
+	public String getType(){ //getter method to return Audio type as String
+		return "CD";  //method returns the Audio type as a String
 	}
 
 }
