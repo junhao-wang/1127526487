914f64118a32cf21a1c7ea4f6c801220862edebd
Colin Vandenhof <colinvandenhof@gmail.com>
2015-11-24 00:43:42 -0500

    Update eBook.java

--- eBook.java
+++ eBook.java
@@ -1,14 +1,14 @@
 
-public class eBook extends Readable{
+public class eBook extends Readable{ //class eBook is a subclass of Readable
 
 	public eBook(int sNo, String name, String authorName, int price, int quantity) {
-		super(sNo, name, authorName, price, quantity);
+		super(sNo, name, authorName, price, quantity); //inherit constructors from Readable
 	}
-	public int getPrice(){
-		return super.getPrice();
+	public int getPrice(){//getter method to return Price of Book as an int.
+		return super.getPrice(); //+super.getPrice()/50;
 	}
-	public String getType(){
-		return "eBook";
+	public String getType(){ //getter method to get Readable type as a String
+		return "eBook"; //method returns the Readable type as a String
 	}
 }
 
